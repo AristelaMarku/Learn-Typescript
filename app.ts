a936e947e9aae4ff085dced93c13924c4bc65648
Aristela Marku <aristela.marku@gmail.com>
2022-03-17 20:56:07 -0400

    object

--- app.ts
+++ app.ts
@@ -1,13 +1,9 @@
-function add(n1: number,n2: number, showResult: boolean, resultPhase: string){
-    if(showResult){
-     console.log(resultPhase + n1 + n2)
-    }else{
-     return n1 + n2
-    }
+const person: {
+    name: string;
+    age:number;
+} = {
+    name: "Aristela",
+    age: 31
 }
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhase = 'Result is:'
 
-add(number1,number2, printResult, resultPhase)
+console.log(person.name)
